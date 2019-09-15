45ba19c16d749729c84250d15d0ad075331cae9d
vasilisdimitriou <vasilisdmr@gmail.com>
2019-09-15 23:15:03 +0300

    Created a test to check if the name of the result view is actually result.

--- ControllersAndActions.Tests/ActionTests.cs
+++ ControllersAndActions.Tests/ActionTests.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using ControllersAndActions.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
 namespace ControllersAndActions.Tests
 {
     public class ActionTests
     {
-        public ActionTests()
+        [Fact]
+        public void ViewSelected()
         {
+            //Arrange
+            HomeController cntrl = new HomeController();
+
+            //Act
+            ViewResult result = cntrl.ReceiveForm("Adam", "London");
+
+            //Assert
+            Assert.Equal("Result", result.ViewName);
         }
     }
 }
