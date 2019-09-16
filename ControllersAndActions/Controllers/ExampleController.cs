a83aab11e3250ddc392a45cd16a35320ac32b1aa
vasilisdimitriou <vasilisdmr@gmail.com>
2019-09-16 22:50:02 +0300

    Just an example controller used to passed a datetime object to the view.

---
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public ViewResult Index() => View(DateTime.Now);
+    }
+}
