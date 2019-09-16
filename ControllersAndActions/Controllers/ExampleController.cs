7381a79c3f3ce80d5fdaf619976f0e804abf992a
vasilisdimitriou <vasilisdmr@gmail.com>
2019-09-16 23:17:11 +0300

    Used viewbag to pass data to default view.

--- ControllersAndActions/Controllers/ExampleController.cs
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -5,9 +5,13 @@ namespace ControllersAndActions.Controllers
 {
     public class ExampleController : Controller
     {
+        public ViewResult Index()
+        {
+            ViewBag.Message = "Hello";
+            ViewBag.Date = DateTime.Now;
+            return View();
+        }
 
-
-
-        public ViewResult Index() => View((object)"Hello Master");
+        public ViewResult Result() => View((object)"Hello Master");
     }
 }
