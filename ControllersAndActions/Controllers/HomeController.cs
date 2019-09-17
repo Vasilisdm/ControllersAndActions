7e49ecbd3f562dd492030d7919482faac74606d6
vasilisdimitriou <vasilisdmr@gmail.com>
2019-09-17 22:51:41 +0300

    Used TempData to pass both name and the city.

--- ControllersAndActions/Controllers/HomeController.cs
+++ ControllersAndActions/Controllers/HomeController.cs
@@ -9,8 +9,18 @@ namespace ControllersAndActions.Controllers
         public ViewResult Index() => View("SimpleForm");
 
         [HttpPost]
-        public RedirectToActionResult ReceiveForm(string name, string city) => RedirectToAction(nameof(Data));
+        public RedirectToActionResult ReceiveForm(string name, string city)
+        {
+            TempData["name"] = name;
+            TempData["city"] = city;
+            return RedirectToAction(nameof(Data)); 
+        }
 
-        public ViewResult Data() => View("Result");
+        public ViewResult Data()
+        {
+            string name = TempData["name"] as string;
+            string city = TempData["city"] as string;
+            return View("Resutl", $"{name} lives in {city}");
+        }
     }
 }
